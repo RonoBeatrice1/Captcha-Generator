b000ef2974a9d315cebcc9495f7c9826ecd426ef
Shikuku39 <enshikuku39@gmail.com>
2023-10-06 12:49:52 +0300

    Refactor captcha functionality and improve code structure

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-var code;
+document.addEventListener('DOMContentLoaded', function () {
+  const captchaForm = document.getElementById('captchaForm');
+  const captchaInput = document.getElementById('captchaInput');
+  const captchaContainer = document.getElementById('captchaContainer');
+  let captchaCode = '';
 
-// Function to create a random Captcha
-function createCaptcha() {
-  //Clear the contents of captcha div first
-  document.getElementById("captcha").innerHTML = "";
-  var charsArray =
-    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!#$%^&*";
-  var lengthOtp = 6;
-  var captcha = [];
-  for (var i = 0; i < lengthOtp; i++) {
-    //Below code will not allow Repetition of Characters
-    var index = Math.floor(Math.random() * charsArray.length + 1); //Get the next character from the array
-    if (captcha.indexOf(charsArray[index]) == -1)
-      captcha.push(charsArray[index]);
-    else i--; // Decrement i to generate a new character if repetition occurs
+  function generateCaptcha() {
+      const charsArray = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@!#$%^&*";
+      const lengthOtp = 6;
+      let captcha = '';
+
+      for (let i = 0; i < lengthOtp; i++) {
+          const index = Math.floor(Math.random() * charsArray.length);
+          captcha += charsArray[index];
+      }
+
+      captchaCode = captcha;
+      updateCaptchaDisplay(captcha);
   }
-  var canv = document.createElement("canvas");
-  canv.id = "captcha";
-  canv.width = 100;
-  canv.height = 50;
-  var ctx = canv.getContext("2d");
-  ctx.font = "25px Georgia";
-  ctx.strokeText(captcha.join(""), 0, 30);
 
-  //Storing captcha so that can validate you can save it somewhere else according to your specific requirements
-  code = captcha.join("");
-  document.getElementById("captcha").appendChild(canv); // Adds the canvas to the body element
-}
-
-// Function to validate the Captcha
-function validateCaptcha() {
-  event.preventDefault();
-  debugger;
-  if (document.getElementById("cpatchaTextBox").value == code) {
-    alert("Valid Captcha");
-  } else {
-    alert("Invalid Captcha. try Again");
-    createCaptcha();
+  function updateCaptchaDisplay(captcha) {
+      captchaContainer.textContent = captcha;
   }
-}
+
+  function validateCaptcha() {
+      event.preventDefault();
+
+      if (captchaInput.value === captchaCode) {
+          alert('Valid Captcha');
+          captchaInput.value = '';
+          generateCaptcha(); // Generate a new CAPTCHA
+      } else {
+          alert('Invalid Captcha. Try Again');
+          captchaInput.value = '';
+          generateCaptcha(); // Generate a new CAPTCHA
+      }
+  }
+
+  generateCaptcha(); // Initial CAPTCHA generation
+  captchaForm.addEventListener('submit', validateCaptcha);
+});
